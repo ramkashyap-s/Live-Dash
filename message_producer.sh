6cfe7956609e334abf97d78b5a55ab77fe2248fb
ramkashyap-s <2968129+ramkashyap-s@users.noreply.github.com>
2019-03-01 02:03:16 -0500

    simple shell

---
+++ message_producer.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#to start sending messages to broker
+python3 run_bot.py
--- src/spark_submit.sh
+++ src/spark_submit.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-#to start sending messages to broker
-python3 run_bot.py
-
 # for submitting spark application to master
 spark-submit \
 --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0,org.postgresql:postgresql:42.2.5  \
 --master spark://<hostname>:7077
-/src/spark_streaming/aggregate.py
+spark_streaming/aggregate.py
 
 # to run spark application locally
 spark-submit \
